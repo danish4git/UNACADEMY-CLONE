e3866dbf41b6948ceaca96472b0030e95deeb3ef
Abhishek Kumar <anu250867anu@gmail.com>
2022-04-02 16:12:52 +0530

    explore page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../components/Home";
 import { CoursePage } from "../components/pages/Coursepage";
+import { ExplorePage } from "../pages/ExplorePage";
 
 const AllRoutes = () => {
   return (
@@ -12,6 +13,8 @@ const AllRoutes = () => {
           path="/jee-main-and-advanced-preparation"
           element={<CoursePage />}
         ></Route>
+
+        <Route path="/explore" element={<ExplorePage />}></Route>
       </Routes>
     </div>
   );
