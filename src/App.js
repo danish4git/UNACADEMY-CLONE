9fae62f8a9282e80ed7c9806562456650fe9d98f
Manish Reddy <89566773+Manish001234@users.noreply.github.com>
2022-04-02 16:45:44 +0530

    payment page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import "./App.css";
-import AllRoutes from "./routes/Routes";
+import logo from './logo.svg';
+import './App.css';
+import GetSubscription from './components/subscription';
+import Plus from './components/plus';
+import { Routes ,Route } from 'react-router-dom';
+import { Payment } from './components/payment';
 
 function App() {
   return (
-    <div className="App">
-      <AllRoutes />
+    <div className="App"> 
+    <Routes>
+      <Route path='/' element={<GetSubscription/>}></Route>
+      <Route path='/Plus' element={<Plus/>}></Route>
+      <Route path ="/payment" element={<Payment/>}></Route>
+    </Routes>
+   
     </div>
   );
 }
