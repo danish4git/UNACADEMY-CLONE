c2e9323c805d0f2b232bd127b9733f63141849bd
Dinesh Sharma <93374756+dinoweblog@users.noreply.github.com>
2022-04-02 15:04:18 +0530

    Course Page & other

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { CoursePage } from "./pages/CoursePage";
+import { Sliders } from "./Sliders/Slider";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <CoursePage />
+    </div>
+  );
 };
 
 export default Home;
