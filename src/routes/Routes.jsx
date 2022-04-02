722e9ff7079c2613226bd972ebcb446e09c8e3c2
Dinesh Sharma <93374756+dinoweblog@users.noreply.github.com>
2022-04-02 15:53:06 +0530

    Update

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../components/Home";
-import { CoursePage } from "../components/pages/Coursepage";
+import { CoursePage } from "../components/pages/CoursePage";
 
 const AllRoutes = () => {
   return (
